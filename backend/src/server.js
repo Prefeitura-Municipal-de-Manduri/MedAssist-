9b977ccecccf18b74a2605893446c829b433d933
MatheusMarcelo1 <matheusmpmenezes@protonmail.com>
2026-03-20 11:19:59 -0300

    add_auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+
 
 import pacientesRoutes from './routes/pacientes.js';
 import medicamentosRoutes from './routes/medicamentos.js';
@@ -14,6 +16,8 @@ app.use(express.json());
 
 app.use('/pacientes', pacientesRoutes);
 app.use('/medicamentos', medicamentosRoutes);
+app.use('/auth', authRoutes);
+
 
 app.listen(3000, () => {
   console.log('🔥 Backend rodando em http://localhost:3000');
